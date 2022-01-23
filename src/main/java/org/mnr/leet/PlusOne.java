e0c60d959494b4d7fd14a1b92adcbd19d399d7d7
Naveen <mnreddy.nv@gmail.com>
2022-01-23 19:35:57 +0530

    leet

--- src/main/java/org/mnr/leet/PlusOne.java
+++ src/main/java/org/mnr/leet/PlusOne.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(plusOne(new int[]{9,8,7,6,5,4,3,2,1,0})));
-        System.out.println(Arrays.toString(plusOne(new int[]{1,2,3})));
-        System.out.println(Arrays.toString(plusOne(new int[]{0,0,-1})));
+        //System.out.println(Arrays.toString(plusOne(new int[]{9,8,7,6,5,4,3,2,1,0})));
+        //System.out.println(Arrays.toString(plusOne(new int[]{1,2,3})));
+        System.out.println(Arrays.toString(plusOne(new int[]{1,9,9,9})));
     }
     public static int[] plusOne(int[] digits) {
         for(int i=digits.length-1;i>=0;i--){
